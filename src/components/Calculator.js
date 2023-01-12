c949fb5dea247a51c196d729501553f65c7c9714
ONJoseph <on.josephinfo@gmail.com>
2023-01-12 07:12:52 +0100

    add clickHandler and import calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,56 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      next: null,
+      operation: null,
+      total: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
+    const { next, operation, total } = this.state;
     return (
       <div className="container">
-        <div className="screen">0</div>
+        <div className="screen">{ next || operation || total || 0 }</div>
         <div className="buttons">
           <div className="row">
-            <div className="gray btn-normal">AC</div>
-            <div className="gray btn-normal">+/-</div>
-            <div className="gray btn-normal">%</div>
-            <div className="first orange btn-normal">÷</div>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">AC</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">+/-</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">%</button>
+            <button type="button" onClick={this.clickHandler} className="first orange btn-normal">÷</button>
           </div>
           <div className="row">
-            <div className="gray btn-normal">7</div>
-            <div className="gray btn-normal">8</div>
-            <div className="gray btn-normal">9</div>
-            <div className="orange btn-normal">x</div>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">7</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">8</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">9</button>
+            <button type="button" onClick={this.clickHandler} className="orange btn-normal">x</button>
           </div>
           <div className="row">
-            <div className="gray btn-normal">4</div>
-            <div className="gray btn-normal">5</div>
-            <div className="gray btn-normal">6</div>
-            <div className="orange btn-normal">-</div>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">4</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">5</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">6</button>
+            <button type="button" onClick={this.clickHandler} className="orange btn-normal">-</button>
           </div>
           <div className="row">
-            <div className="gray btn-normal">1</div>
-            <div className="gray btn-normal">2</div>
-            <div className="gray btn-normal">3</div>
-            <div className="orange btn-normal">+</div>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">1</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">2</button>
+            <button type="button" onClick={this.clickHandler} className="gray btn-normal">3</button>
+            <button type="button" onClick={this.clickHandler} className="orange btn-normal">+</button>
           </div>
           <div className="row">
-            <div className=" double gray btn-normal">0</div>
-            <div className="last gray btn-normal">.</div>
-            <div className="last orange btn-normal">=</div>
+            <button type="button" onClick={this.clickHandler} className=" double gray btn-normal">0</button>
+            <button type="button" onClick={this.clickHandler} className="last gray btn-normal">.</button>
+            <button type="button" onClick={this.clickHandler} className="last oraange btn-normal">=</button>
           </div>
         </div>
       </div>
